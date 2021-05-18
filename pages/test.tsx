828b859ee70f0266458f54ba10d57ae1d9e0281c
maru44 <takumaru22244@gmail.com>
2021-05-19 07:45:57 +0900

    for go s3 sample

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,11 @@
 import { NextPage } from "next";
+import { useState } from "react";
+import { BACKEND_URL } from "../helper/Config";
 
 const Test: NextPage = () => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState(null);
+
   const fileReader = (file: any) =>
     new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -10,11 +15,21 @@ const Test: NextPage = () => {
     });
 
   const consoleChange = async (e: any) => {
-    const file = e.target.files[0];
-    const fileName = file.name;
-    console.log(fileName);
-    const readed = await fileReader(file);
-    console.log(readed);
+    const fileTmp = e.target.files[0];
+    const fileNameTmp = fileTmp.name;
+    const readed = await fileReader(fileTmp);
+    setFileName(fileNameTmp);
+    setFile(readed);
+  };
+
+  const startUpload = async () => {
+    const res = await fetch(`${BACKEND_URL}/test/upload/`, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({ file: file, filename: fileName }),
+    });
+    const ret = await res.json();
+    console.log(ret);
   };
 
   return (
@@ -25,6 +40,11 @@ const Test: NextPage = () => {
         accept="image/png, image/jpeg"
         name="testimage"
       />
+      <div>
+        <button className="" onClick={startUpload}>
+          画像アップロード
+        </button>
+      </div>
     </div>
   );
 };
