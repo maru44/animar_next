220388dc69d3507f79868e2cc29c70700f248d5e
maru44 <takumaru22244@gmail.com>
2021-06-20 12:58:16 +0900

    remove inquiry

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,11 +10,6 @@ const Footer: NextPage = () => {
             <h5>&copy;2021 Maru-t</h5>
           </div>
           <div className="mla pt20">
-            <Link href="https://twitter.com/intent/tweet?hashtags=loveanime,ラブアニメお問合せ">
-              <a target="_new" rel="nofollow">
-                お問合せ
-              </a>
-            </Link>
             <Link href="https://twitter.com/intent/tweet?hashtags=loveanime,ラブアニメご要望">
               <a className="ml20" target="_new" rel="nofollow">
                 ご要望
