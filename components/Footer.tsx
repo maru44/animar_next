95f6449f6de1581caaeca9b7e480d6bf1645dbc3
maru44 <takumaru22244@gmail.com>
2021-06-20 12:23:34 +0900

    inquiry demand

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,18 @@ const Footer: NextPage = () => {
           <div className="footerCopy pt20">
             <h5>&copy;2021 Maru-t</h5>
           </div>
+          <div className="mla pt20">
+            <Link href="https://twitter.com/intent/tweet?hashtags=loveanime,ラブアニメお問合せ">
+              <a target="_new" rel="nofollow">
+                お問合せ
+              </a>
+            </Link>
+            <Link href="https://twitter.com/intent/tweet?hashtags=loveanime,ラブアニメご要望">
+              <a className="ml20" target="_new" rel="nofollow">
+                ご要望
+              </a>
+            </Link>
+          </div>
         </div>
       </footer>
     </div>
