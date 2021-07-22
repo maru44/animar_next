4a93e50234ea532c5e1d4fd75ab36c48fc839857
maru44 <takumaru22244@gmail.com>
2021-07-22 17:30:40 +0900

    Image for production

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = {
     return config;
   },
   images: {
-    domains: [process.env.NEXT_PUBLIC_S3_DOMAIN],
+    domains: [
+      "loveanime.s3.ap-northeast-1.amazonaws.com",
+      process.env.NEXT_PUBLIC_S3_DOMAIN,
+    ],
   },
 };
