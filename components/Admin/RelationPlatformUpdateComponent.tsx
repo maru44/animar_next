2eb0b915d6e1ded7b0517745e7aaebef3b9fa8da
maru44 <takumaru22244@gmail.com>
2021-10-19 22:14:18 +0900

    relplatupdate component

--- components/Admin/RelationPlatformUpdateComponent.tsx
+++ components/Admin/RelationPlatformUpdateComponent.tsx
@@ -17,37 +17,6 @@ const RelationPlatformUpdateComponent = (props: props) => {
       <div>
         <div className="modal" onClick={props.closeFunc}></div>
         <div className="modalCon">
-          {/* <form
-            className=""
-            onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
-              startUpdateRelationPlatform(e, plat.anime_id)
-            }
-          >
-            <input type="hidden" name="plat" value={plat.platform_id} />
-            <h3>{plat && plat.plat_name}</h3>
-            <div className="field mt20">
-              <input type="text" name="url" defaultValue={plat.link_url} />
-            </div>
-            <div className="field mt20">
-              <label>初回放送日時</label>
-              <input type="datetime-local" name="fb" defaultValue={fDay()} />
-            </div>
-            <div className="field mt20">
-              <select name="interval" defaultValue={plat.interval}>
-                <option value="">------------</option>
-                {listInterval.map((v: string, i: number) => (
-                  <option key={i} value={v}>
-                    {v}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="field mt20">
-              <button className="" type="submit">
-                編集する
-              </button>
-            </div>
-          </form> */}
           <RelationPlatformComponent
             relPlat={plat}
             animeId={plat.anime_id}
